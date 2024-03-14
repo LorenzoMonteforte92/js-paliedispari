20f0548406f8c5e71efb6c90fc4fa903a0c981f6
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-14 18:39:31 +0100

    feat: add prompt and palindrome function

--- Palindroma/JS/script.js
+++ Palindroma/JS/script.js
@@ -2,13 +2,15 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+//chiedo la parola all'utente
+let userWord = prompt(`inserisci una parola`)
+//leggere la parola nel da sinistra verso destra
+//leggerla da destra verso sinistra
+// confronto se la parola scritta al contrario è uguale a quella inserita dall'utente
 
+let palidrome = isWordPalindrome (userWord);
 
-
-
-
-
-
+console.log(palidrome)
 
 
 
@@ -16,3 +18,12 @@
 // ----------------
 //     FUNCTIONS
 // ----------------
+
+function isWordPalindrome (word){
+    
+    let wordIndex = ``
+    for(let i = 0; i < word.length; i++) {
+        wordIndex += word[i]
+    }
+    return wordIndex
+}
