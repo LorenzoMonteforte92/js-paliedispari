b731f42a0438bb8187c3205ccf120d222a7de965
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-14 17:14:17 +0100

    feat: odd or even start

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+//chiedere all'utente di scegliere pari o dispari
+let userChoiche = prompt(`Scegli pari o dispari`);
+//chiedere di scegliere un numero da uno a 5
+let userNumber = parseInt(prompt(`Scegli un numero da 1 a 5`));
