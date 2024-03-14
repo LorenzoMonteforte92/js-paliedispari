6831e74c45f3360e93319fad3845a548378a4131
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-14 17:45:17 +0100

    feat: add prompt and validation

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,18 @@
 // Dichiariamo chi ha vinto.
 
 //chiedere all'utente di scegliere pari o dispari
-let userChoiche = prompt(`Scegli pari o dispari`);
+let userChoiche
+//valido i prompt 
+while(userChoiche != "pari" && userChoiche != "dispari"){
+    userChoiche = prompt(`Scegli pari o dispari`);
+}
+console.log(userChoiche);
 //chiedere di scegliere un numero da uno a 5
 let userNumber = parseInt(prompt(`Scegli un numero da 1 a 5`));
+//valido i prompt
+while(userNumber < 1 || userNumber >= 5 ){
+    userNumber = parseInt(prompt(`Scegli un numero da 1 a 5`));
+}
+
+console.log(userNumber);
+
